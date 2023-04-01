a6ea675815da8484c6d49b6f83c237fe91162e3c
Domin <domin@is-a.tech>
2023-04-01 17:11:27 +0300

    Moving to docs.danbot.space
    Added a redirect line instead of rendering anything

--- views/js/app.js
+++ views/js/app.js
@@ -36,68 +36,70 @@ app.use(express.static("./public", {
 }));
 
 // /issue-tracker/id/[ID/name] returns post, IDs start from 1
-app.get('/issue-tracker', (req, res) => {
-    const directory = './docs/issue-tracker/id';
-    let converted = '# Issue Tracker\nAll known DBH issues and if possible, their solutions.\nt{**Note**: This blog RSS may not contain all errors and issues. Contact contributors for any changes or consider expanding it manually through [GitHub repository](//github.com/DBH-Docs/Documentation/).}<ul class="issue-tracker">';
-    let description = 'All known DBH issues and if possible, their solutions.';
-    fs.readdir(directory, (err, files) => {
-        if (err) console.log(dbh.defineMessage('ERR', err));
-        let count = 1;
-        files.sort(function(a, b) { return a.slice(0, -3) - b.slice(0, -3); })
-            .reverse()
-            .forEach(file => {
-                converted += `<li id="${count}'"><hr>\n` + fs.readFileSync(directory + '/' + file, 'utf8') + '</li>';
-                count++;
-            });
-    });
-    fs.readFile('./views/summary.md', 'utf8', (err, data) => {
-        if (err) console.log(dbh.defineMessage('ERR', err));
-        let items = converter.makeHtml(data);
-        converted += '</ul>\n<btn>Load more...</btn>';
-        converted = converter.makeHtml(converted);
-        let np = dbh.npFromSummary(req.url);
-        res.status(200).render('ejs/200', {
-            content: converted,
-            formatNextPage: np[1],
-            iconNextPage: np[2],
-            logo: dbh.randomLogo(),
-            menu: items,
-            menuFormatted: dbh.filterSummary(items),
-            metaDescription: description,
-            nextPage: np[0]
-        });
-    });
-});
+// app.get('/issue-tracker', (req, res) => {
+//     const directory = './docs/issue-tracker/id';
+//     let converted = '# Issue Tracker\nAll known DBH issues and if possible, their solutions.\nt{**Note**: This blog RSS may not contain all errors and issues. Contact contributors for any changes or consider expanding it manually through [GitHub repository](//github.com/DBH-Docs/Documentation/).}<ul class="issue-tracker">';
+//     let description = 'All known DBH issues and if possible, their solutions.';
+//     fs.readdir(directory, (err, files) => {
+//         if (err) console.log(dbh.defineMessage('ERR', err));
+//         let count = 1;
+//         files.sort(function(a, b) { return a.slice(0, -3) - b.slice(0, -3); })
+//             .reverse()
+//             .forEach(file => {
+//                 converted += `<li id="${count}'"><hr>\n` + fs.readFileSync(directory + '/' + file, 'utf8') + '</li>';
+//                 count++;
+//             });
+//     });
+//     fs.readFile('./views/summary.md', 'utf8', (err, data) => {
+//         if (err) console.log(dbh.defineMessage('ERR', err));
+//         let items = converter.makeHtml(data);
+//         converted += '</ul>\n<btn>Load more...</btn>';
+//         converted = converter.makeHtml(converted);
+//         let np = dbh.npFromSummary(req.url);
+//         res.status(200).render('ejs/200', {
+//             content: converted,
+//             formatNextPage: np[1],
+//             iconNextPage: np[2],
+//             logo: dbh.randomLogo(),
+//             menu: items,
+//             menuFormatted: dbh.filterSummary(items),
+//             metaDescription: description,
+//             nextPage: np[0]
+//         });
+//     });
+// });
 
 // Redirect to /[page]/ if /[page]
 // Each request reads for markdown file in docs folder.
 // Converts md to html to respond with proper format.
 app.get('*', (req, res) => {
-    let url = req.url;
-    if (!url.endsWith('/')) return res.redirect(url + '/');
-    if (url == '/') return res.redirect('/introduction/');
-    fs.readFile('./docs' + url.slice(0, -1) + '.md', 'utf8' , (err, data) => {
-    	if (err) data = '# Page not found, 404!\nI found nothing... Here\'s a picture of some node.js dev duck\n![An image of node.js dev duck by devducks](//cdn.shopify.com/s/files/1/1321/6369/products/DSCF1962_412x412.jpg)'
-        let converted = converter.makeHtml(data);
-        let description = /<p>(.*?)<\/p>/.exec(converted);
-        description = (description) ? description[1] : "";
-        description = description.replace(/(<([^>]+)>)/ig, '');
-        fs.readFile('./views/summary.md', 'utf8', (err, data) => {
-    		if (err) console.log(dbh.defineMessage('ERR', err));
-        	let items = converter.makeHtml(data);
-            let np = dbh.npFromSummary(url);
-            res.status(200).render('ejs/200', {
-                content: converted,
-                formatNextPage: np[1],
-                iconNextPage: np[2],
-                logo: dbh.randomLogo(),
-                menu: items,
-                menuFormatted: dbh.filterSummary(items),
-                metaDescription: description,
-                nextPage: np[0]
-            });
-    	});
-    });
+    // Moving to https://github.com/danbotspace/docs
+    res.redirect("https://docs.danbot.space" + req.url);
+//     let url = req.url;
+//     if (!url.endsWith('/')) return res.redirect(url + '/');
+//     if (url == '/') return res.redirect('/introduction/');
+//     fs.readFile('./docs' + url.slice(0, -1) + '.md', 'utf8' , (err, data) => {
+//     	if (err) data = '# Page not found, 404!\nI found nothing... Here\'s a picture of some node.js dev duck\n![An image of node.js dev duck by devducks](//cdn.shopify.com/s/files/1/1321/6369/products/DSCF1962_412x412.jpg)'
+//         let converted = converter.makeHtml(data);
+//         let description = /<p>(.*?)<\/p>/.exec(converted);
+//         description = (description) ? description[1] : "";
+//         description = description.replace(/(<([^>]+)>)/ig, '');
+//         fs.readFile('./views/summary.md', 'utf8', (err, data) => {
+//     		if (err) console.log(dbh.defineMessage('ERR', err));
+//         	let items = converter.makeHtml(data);
+//             let np = dbh.npFromSummary(url);
+//             res.status(200).render('ejs/200', {
+//                 content: converted,
+//                 formatNextPage: np[1],
+//                 iconNextPage: np[2],
+//                 logo: dbh.randomLogo(),
+//                 menu: items,
+//                 menuFormatted: dbh.filterSummary(items),
+//                 metaDescription: description,
+//                 nextPage: np[0]
+//             });
+//     	});
+//     });
 });
 
 // Starting the web.
